7ffee4763f7d06f2078cbef9fad451545737ade1
Youssef Youssef <youssefyoussef28@gmail.com>
2023-08-11 09:58:38 +0300

    Setting up the Youtube Form and Adding RHF

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { YouTubeForm } from "./assets/components/YouTubeForm";
+
+function App() {
+  return (
+    <>
+      <div>
+        <YouTubeForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/YouTubeForm.tsx
@@ -0,0 +1,25 @@
+import { useForm } from "react-hook-form";
+
+export const YouTubeForm = () => {
+  const form = useForm();
+  const { register } = form;
+
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" {...register("username")} />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" {...register("email")} />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register("channel")} />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
